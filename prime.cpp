21ca2ca7df1b923852122321e231242b7b9cc89b
Bhrugu0516 <bhruguchaudhari1605@gmail.com>
2025-02-08 18:38:43 +0530

    code

--- prime.cpp
+++ prime.cpp
@@ -2,30 +2,45 @@
 using namespace std;
 
 int primecheck(int n){
-    int prime=1;
     for(int i=2;i<n;i++){
         if(n==0 || n==1){
-            continue;
+            cout <<"it is no prime number"<<endl;
+            break;
         }
      
-     else if (n/i==0){
-        prime =0;
+     else if (n%i==0){
+        cout <<"it is not prime number "<<endl;
+        break;
      }
      else {
-        prime=1;
+        cout << "it is a prime number"<< endl;
+        break;
      }
 
 }
-if (prime==1){
-    cout <<"it is prime number";
 }
-else 
-cout <<"not a prime number";
+
+int primeprint(int n){
+    cout << "2"<<" ";
+    for(int i=2;i<n;i++){
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+             break;
+            }
+            else{
+                cout << i <<" ";
+                break;
+            }
+        }
+    }
 }
+
+
 int main(){
     int n;
     cin >> n;
     primecheck(n);
+    primeprint(n);
 
     return 0;
-}
+}  
